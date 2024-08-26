f9f8f08f5364de77f5d8448a18d6dd1bc9465cdb
doonyango <datieno001@gmail.com>
2024-08-26 19:25:52 +0300

    feat: add static files handler

--- internals/routers/routes.go
+++ internals/routers/routes.go
@@ -2,10 +2,16 @@ package routers
 
 import (
 	"net/http"
+	"path/filepath"
 
 	"github.com/Doreen-Onyango/groupie-tracker/internals/handlers"
 )
 
 func RegisterRoutes() {
 	http.HandleFunc("/", handlers.HomeHandler)
+
+	staticDir := http.Dir(filepath.Join("../../static"))
+	staticHandler := http.StripPrefix("/static/", http.FileServer(staticDir))
+
+	http.Handle("/static/", staticHandler)
 }
