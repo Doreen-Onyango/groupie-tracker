abad829b4b03d50b466ae19ec4521d5b82425669
doonyango <datieno001@gmail.com>
2024-08-26 19:25:12 +0300

    fix: dynmically get project root

--- internals/handlers/home_handler.go
+++ internals/handlers/home_handler.go
@@ -3,10 +3,14 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/Doreen-Onyango/groupie-tracker/pkg/helpers"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("../../static/index.html")
+	tmplPath := helpers.Getprojectroute("static/index.html")
+
+	tmpl, err := template.ParseFiles(tmplPath)
 	if err != nil {
 		http.Error(w, "Opps! Something went wrong", http.StatusInternalServerError)
 		return
