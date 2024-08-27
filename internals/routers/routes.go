a2bcc2821fb1ffad3ecf98dbca14a4747779b0a6
doonyango <datieno001@gmail.com>
2024-08-27 16:47:29 +0300

    feat: add data path

--- internals/routers/routes.go
+++ internals/routers/routes.go
@@ -9,6 +9,7 @@ import (
 
 func RegisterRoutes() {
 	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/data", handlers.GetData)
 
 	staticDir := http.Dir(filepath.Join("../../static"))
 	staticHandler := http.StripPrefix("/static/", http.FileServer(staticDir))
