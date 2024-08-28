cc3032995a5205ed9c2f711cf7bdf4183fbf0b78
doonyango <datieno001@gmail.com>
2024-08-28 17:26:27 +0300

    ref: use handler as a method - for scalability

--- internals/handlers/get_data_handler.go
+++ internals/handlers/get_data_handler.go
@@ -5,13 +5,11 @@ import (
 	"html/template"
 	"net/http"
 
-	"github.com/Doreen-Onyango/groupie-tracker/internals/models"
 	"github.com/Doreen-Onyango/groupie-tracker/pkg/helpers"
 )
 
-func GetData(w http.ResponseWriter, r *http.Request) {
-	models.NewMainApi()
-	res, _ := models.FetchData("https://groupietrackers.herokuapp.com/api/artists")
+func (h *Repo) GetData(w http.ResponseWriter, r *http.Request) {
+	res, _ := h.api.FetchData("artists")
 
 	var dataInfo interface{}
 	_ = json.Unmarshal(res, &dataInfo)
