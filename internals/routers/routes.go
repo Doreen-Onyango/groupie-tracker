697cc32fbfd7b2c5bebb8188a2f07f79f64efaa2
doonyango <datieno001@gmail.com>
2024-08-28 17:29:32 +0300

    chore: receive Repo data - for scalability

--- internals/routers/routes.go
+++ internals/routers/routes.go
@@ -7,9 +7,9 @@ import (
 	"github.com/Doreen-Onyango/groupie-tracker/internals/handlers"
 )
 
-func RegisterRoutes() {
-	http.HandleFunc("/", handlers.HomeHandler)
-	http.HandleFunc("/data", handlers.GetData)
+func RegisterRoutes(repo *handlers.Repo) {
+	http.HandleFunc("/", repo.HomeHandler)
+	http.HandleFunc("/data", repo.GetData)
 
 	staticDir := http.Dir(filepath.Join("../../static"))
 	staticHandler := http.StripPrefix("/static/", http.FileServer(staticDir))
