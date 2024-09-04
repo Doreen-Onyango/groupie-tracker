c931c55fa15fc075847177410700ad80e6921d46
doonyango <datieno001@gmail.com>
2024-09-04 15:23:52 +0300

    feat: implement Api model

---
+++ backend/internals/models/api_models.go
@@ -0,0 +1,15 @@
+package models
+
+type MainApi struct {
+	apiMap map[string]string
+}
+
+func NewMainApi() *MainApi {
+	apiMap := map[string]string{
+		"artists":   "https://groupietrackers.herokuapp.com/api/artists",
+		"locations": "https://groupietrackers.herokuapp.com/api/locations",
+		"dates":     "https://groupietrackers.herokuapp.com/api/dates",
+		"relation":  "https://groupietrackers.herokuapp.com/api/relation",
+	}
+	return &MainApi{apiMap}
+}
