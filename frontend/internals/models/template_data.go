118858cc077cccd759abbf6c0ba0c1ecbcd40e4d
adiozdaniel <adiozdaniel@gmail.com>
2024-09-04 20:50:53 +0300

    chore: create template cache - for system effeciency

--- frontend/internals/models/template_data.go
+++ frontend/internals/models/template_data.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"fmt"
 	"html/template"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // TemplateData holds configuration for the application.
@@ -12,4 +16,37 @@ type TemplateData struct {
 // NewTemplateData returns a new instance of TemplateData.
 func NewTemplateData() *TemplateData {
 	return &TemplateData{PageCache: make(map[string]*template.Template)}
+}
+
+// CreateTemplateCache caches all HTML templates.
+func (t *TemplateData) CreateTemplateCache() error {
+	templatesDir := t.GetProjectRoute("views/templates", "*.page.html")
+	pages, err := filepath.Glob(templatesDir)
+	if err != nil {
+		return fmt.Errorf("error globbing templates: %v", err)
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return fmt.Errorf("error parsing page %s: %v", name, err)
+		}
+
+		layoutsPath := t.GetProjectRoute("views/templates", "*.layout.html")
+		matches, err := filepath.Glob(layoutsPath)
+		if err != nil {
+			return fmt.Errorf("error finding layout files: %v", err)
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(layoutsPath)
+			if err != nil {
+				return fmt.Errorf("error parsing layout files: %v", err)
+			}
+		}
+
+		t.PageCache[name] = ts
+	}
+	return nil
 }
