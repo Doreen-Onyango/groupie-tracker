02ebd50f4e18d1aac6e30601fcf2e78bca4fe2fc
adiozdaniel <adiozdaniel@gmail.com>
2024-09-04 20:59:23 +0300

    chore: add render method - for reusability

--- frontend/internals/renders/renders.go
+++ frontend/internals/renders/renders.go
@@ -1,6 +1,9 @@
 package renders
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/Doreen-Onyango/groupie-tracker-client/internals/models"
 )
 
@@ -10,4 +13,20 @@ type Renders struct {
 
 func NewRenders(app *models.App) *Renders {
 	return &Renders{app: app}
+}
+
+// RenderTemplate renders the HTML template specified by `page`
+func (r *Renders) RenderTemplate(page string, status int, w http.ResponseWriter) error {
+	tmpl, err := r.app.TemplateData.GetTemplate(page)
+	if err != nil {
+		return fmt.Errorf("error parsing project template: %w", err)
+	}
+
+	w.WriteHeader(status)
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		return fmt.Errorf("error executing template: %w", err)
+	}
+
+	return nil
 }
