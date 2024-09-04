dd4cf1f3d9519a9c0e7232f0980be92d74c705ee
doonyango <datieno001@gmail.com>
2024-09-04 17:38:20 +0300

    feat: implement json response

---
+++ backend/internals/responses/config.go
@@ -0,0 +1,13 @@
+package responses
+
+import "encoding/json"
+
+type JSONRes struct {
+	Err     bool            `json:"error"`
+	Message string          `json:"message"`
+	Data    json.RawMessage `json:"data,omitempty"`
+}
+
+func NewJSONRes() *JSONRes {
+	return &JSONRes{}
+}
