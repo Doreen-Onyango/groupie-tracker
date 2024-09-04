cd674d02c500ea1cd8fa5d23e99aa9c226b46052
adiozdaniel <adiozdaniel@gmail.com>
2024-09-04 20:43:55 +0300

    chore: initialize handlers

--- frontend/internals/handlers/config.go
+++ frontend/internals/handlers/config.go
@@ -1,11 +1,15 @@
 package handlers
 
-import "github.com/Doreen-Onyango/groupie-tracker-client/internals/models"
+import (
+	"github.com/Doreen-Onyango/groupie-tracker-client-client/internals/models"
+	"github.com/Doreen-Onyango/groupie-tracker-client-client/internals/renders"
+)
 
 type Repo struct {
-	api *models.MainApi
+	app *models.ModelState
+	res *renders.Renders
 }
 
-func NewRepo(apis *models.MainApi) *Repo {
-	return &Repo{apis}
+func NewRepo(app *models.ModelState, res *renders.Renders) *Repo {
+	return &Repo{app: app, res: res}
 }
