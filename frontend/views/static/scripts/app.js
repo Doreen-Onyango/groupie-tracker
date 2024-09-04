076abae4ae3ec78a92f13aaf58786f60005c90cd
adiozdaniel <adiozdaniel@gmail.com>
2024-09-04 20:27:28 +0300

    chore: control data flow - as a communication bridge btw data and rendering

---
+++ frontend/views/static/scripts/app.js
@@ -0,0 +1,23 @@
+const updateHomePage = async (queryParams) => {
+	const { error, message, data } = await getArtistsData(queryParams);
+	return {
+		err: error,
+		msg: message,
+		data: data,
+	};
+};
+
+const searchForm = document.querySelector("form");
+
+searchForm.addEventListener("submit", async function (e) {
+	e.preventDefault();
+
+	const queryParams = document.getElementById("search").value.trim();
+	searchForm.reset();
+
+	updateHomePage(queryParams)
+		.then(({ data }) => {
+			console.log(data);
+		})
+		.catch((err) => console.log(err));
+});
