a8cf03847b789867cc7c0ba31235309a89713916
adiozdaniel <adiozdaniel@gmail.com>
2024-09-07 18:58:13 +0300

    feat: add filtering

--- frontend/views/static/scripts/app.js
+++ frontend/views/static/scripts/app.js
@@ -9,3 +9,19 @@ document.addEventListener("DOMContentLoaded", async function () {
 		console.log(err);
 	}
 });
+
+document.getElementById("search").addEventListener("input", function () {
+	const query = this.value.toLowerCase();
+	const cards = document.querySelectorAll("#artistsContainer .artist-card");
+
+	cards.forEach(function (card) {
+		const artistName = card
+			.querySelector(".artist-name")
+			.textContent.toLowerCase();
+		if (artistName.includes(query)) {
+			card.style.display = "block";
+		} else {
+			card.style.display = "none";
+		}
+	});
+});
