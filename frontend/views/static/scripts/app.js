b59e2a7a5994c3fab69b1cfc25ca2c8e36b48fdd
adiozdaniel <adiozdaniel@gmail.com>
2024-09-08 16:28:13 +0300

    feat:add an event listener to card id

--- frontend/views/static/scripts/app.js
+++ frontend/views/static/scripts/app.js
@@ -1,36 +1,30 @@
 import { getAllArtists } from "/static/scripts/getallartists.js";
 import { getArtistById } from "./getartistbyid.js";
-import { handleData, showModal } from "/static/scripts/helpers.js";
+import { renderAllArtists, showModal } from "./renders.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
 	try {
 		const data = await getAllArtists();
-		handleData(data.data);
+		renderAllArtists(data.data);
 	} catch (err) {
 		console.log(err);
 	}
 });
 
 document.addEventListener("click", async function (event) {
-	// Check if the clicked element is within an artist card link
 	const artistLink = event.target.closest(".artist-card-link");
-	if (!artistLink) return; // Exit if the click was not on a card link
+	if (!artistLink) return;
 
-	// Prevent the default action of the link
 	event.preventDefault();
 
-	// Get the artist ID from the data attribute on the card
 	const artistId = artistLink
 		.querySelector(".artist-card")
 		.getAttribute("data-artist-id");
 
-	if (!artistId) return; // Exit if no artist ID is found
+	if (!artistId) return;
 
 	try {
-		// Fetch the artist data by ID
 		const data = await getArtistById(artistId);
-
-		// Handle the data (e.g., display it in a detailed view)
 		showModal(data.data);
 	} catch (err) {
 		console.error("Error fetching artist details:", err);
