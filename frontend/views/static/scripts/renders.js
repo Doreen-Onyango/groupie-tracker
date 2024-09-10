7da4bb2e6a03f71d08c36cebacb6448ba9f070ce
adiozdaniel <adiozdaniel@gmail.com>
2024-09-10 17:51:41 +0300

    feat:helper function to closemodal - to avoid code repetition

--- frontend/views/static/scripts/renders.js
+++ frontend/views/static/scripts/renders.js
@@ -2,24 +2,21 @@
  * Renders all artists or displays an error message if there's an issue
  * @param {Object} artistsData - The data object containing artists, message, and error status
  */
-export const renderAllArtists = (artistsData) => {
-	const { data, message, error } = artistsData;
+export const renderAllArtists = ({ data, message, error }) => {
 	const container = document.querySelector("#artistsContainer");
 	const template = document.getElementById("artistCardTemplate");
 
-	container.innerHTML = "";
-
-	if (error) {
-		container.innerHTML = `
-			<div class="error-message">
+	container.innerHTML = error
+		? `<div class="error-message">
 				<h2>Oops, there is a network issue!</h2>
 				<p>${message}</p>
-			</div>
-		`;
-		return;
-	}
+			</div>`
+		: "";
+
+	if (error) return;
 
 	const sortedArtistData = sortById(data);
+
 	sortedArtistData.forEach((artist) => {
 		const card = template.content.cloneNode(true);
 
@@ -43,6 +40,7 @@ export const renderAllArtists = (artistsData) => {
 			artist.creationDate || "Unknown Creation Date";
 		card.querySelector(".artist-firstAlbum").textContent =
 			formatDate(artist.firstAlbum) || "Unknown First Album";
+
 		container.appendChild(card);
 	});
 };
@@ -50,23 +48,18 @@ export const renderAllArtists = (artistsData) => {
 /**
  * Displays a modal with detailed information about an artist
  * @param {Object} artistData - Contains details about the artist and associated data
- * This function updates the modal's content with the artist's image, name, creation date, first album, members, locations, concert dates, and relations.
  */
 export function showModal(artistData) {
 	const { data, message, error } = artistData;
 	const modal = document.getElementById("artistDetailsModal");
 	const artistDetailsSection = document.querySelector("#artistDetails");
 
-	if (error) {
-		artistDetailsSection.innerHTML = `
-			<div class="error-message">
+	artistDetailsSection.innerHTML = error
+		? `<div class="error-message">
 				<h2>Oops, there is a network issue!</h2>
 				<p>${message}</p>
-			</div>
-		`;
-	} else {
-		artistDetailsSection.innerHTML = generateArtistDetailsHTML(data);
-	}
+			</div>`
+		: generateArtistDetailsHTML(data);
 
 	modal.classList.add("show");
 
@@ -74,63 +67,76 @@ export function showModal(artistData) {
 	modalContent.classList.add("show");
 
 	const closeButton = document.querySelector(".close-button");
-	closeButton.onclick = function () {
-		modal.classList.remove("show");
-		modalContent.classList.remove("show");
-	};
+	closeButton.onclick = () => closeModal(modal, modalContent);
 
-	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.classList.remove("show");
-			modalContent.classList.remove("show");
-		}
+	window.onclick = (event) => {
+		if (event.target === modal) closeModal(modal, modalContent);
 	};
 }
 
 /**
+ * Closes the modal and hides the modal content
+ * @param {Element} modal - The modal element
+ * @param {Element} modalContent - The modal content element
+ */
+function closeModal(modal, modalContent) {
+	modal.classList.remove("show");
+	modalContent.classList.remove("show");
+}
+
+/**
  * Generates the HTML content for artist details.
- * @param {Object} artist - The artist data to display.
+ * @param {Object} data - The artist data to display.
  * @returns {string} - The generated HTML string.
  */
-function generateArtistDetailsHTML(data) {
+function generateArtistDetailsHTML({
+	artist,
+	locations,
+	concertDates,
+	relations,
+}) {
 	return `
 		<div>
-			<img src="${data.artist.image}" alt="${data.artist.name} image" />
-			<h2>${data.artist.name}</h2>
-			<p><strong>Creation Date:</strong> ${data.artist.creationDate || "Unknown"}</p>
+			<img src="${artist.image}" alt="${artist.name} image" />
+			<h2>${artist.name}</h2>
+			<p><strong>Creation Date:</strong> ${artist.creationDate || "Unknown"}</p>
 			<p><strong>First Album:</strong> ${
-				formatDate(data.artist.firstAlbum) || "Unknown"
+				formatDate(artist.firstAlbum) || "Unknown"
 			}</p>
 		</div>
 		<div>
 			<strong>Members:</strong>
 			<ul id="artistMembersList">
-				${data.artist.members.map((member) => `<li>${member}</li>`).join("")}
+				${artist.members.map((member) => `<li>${member}</li>`).join("")}
 			</ul>
 			<p><strong>Locations:</strong>
 				${
-					data.locations.locations
-						.map((location) => formatLocation(location))
-						.join("") || "<li>No locations set at the moment</li>"
+					locations.locations.length
+						? locations.locations.map(formatLocation).join("")
+						: "<li>No locations set at the moment</li>"
 				}
 			</p>
 			<p><strong>Concert Dates:</strong></p>
 			<ul>
 				${
-					data.concertDates.dates.map((date) => formatDate(date)).join("") ||
-					"<li>No concert dates set at the moment</li>"
+					concertDates.dates.length
+						? concertDates.dates.map(formatDate).join("")
+						: "<li>No concert dates set at the moment</li>"
 				}
 			</ul>
 			<p><strong>Relations:</strong></p>
 			<ul>
 				${
-					Object.entries(data.relations.datesLocations)
-						.map(([location, dates]) => {
-							return `<li>${formatLocation(location)}: ${dates
-								.map((date) => formatDate(date))
-								.join(", ")}</li>`;
-						})
-						.join("") || "<li>No relations set at the moment</li>"
+					Object.entries(relations.datesLocations).length
+						? Object.entries(relations.datesLocations)
+								.map(
+									([location, dates]) =>
+										`<li>${formatLocation(location)}: ${dates
+											.map(formatDate)
+											.join(", ")}</li>`
+								)
+								.join("")
+						: "<li>No relations set at the moment</li>"
 				}
 			</ul>
 		</div>
@@ -163,16 +169,12 @@ function formatLocation(location) {
  */
 function formatDate(date) {
 	const [day, month, year] = date.split("-");
-	const longDate = new Date(`${year}-${month}-${day}`).toLocaleDateString(
-		"en-US",
-		{
-			weekday: "long",
-			year: "numeric",
-			month: "long",
-			day: "numeric",
-		}
-	);
-	return longDate;
+	return new Date(`${year}-${month}-${day}`).toLocaleDateString("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	});
 }
 
 /**
