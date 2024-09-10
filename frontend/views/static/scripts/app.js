69a874dfa45526eb03656dff0d52f8d20266fa7e
adiozdaniel <adiozdaniel@gmail.com>
2024-09-10 10:05:36 +0300

    chores: add documentantion

--- frontend/views/static/scripts/app.js
+++ frontend/views/static/scripts/app.js
@@ -2,6 +2,11 @@ import { getAllArtists } from "/static/scripts/getallartists.js";
 import { getArtistById } from "./getartistbyid.js";
 import { renderAllArtists, showModal } from "./renders.js";
 
+/**
+ * Event listener for when the DOM content is fully loaded
+ * @param {Object} data - Data from the getAllArtists() call
+ * Manipulates the DOM to display the artists.
+ */
 document.addEventListener("DOMContentLoaded", async function () {
 	try {
 		const data = await getAllArtists();
@@ -11,6 +16,12 @@ document.addEventListener("DOMContentLoaded", async function () {
 	}
 });
 
+/**
+ * Event listener for individual artist card click events
+ * @param {Event} event - Click event on an artist card
+ * @param {Object} data - Data from the getArtistById() call
+ * Retrieves artist details and displays them in a modal.
+ */
 document.addEventListener("click", async function (event) {
 	const artistLink = event.target.closest(".artist-card-link");
 	if (!artistLink) return;
