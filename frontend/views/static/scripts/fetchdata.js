68cc0be198db0e30198cfc26fe1ed861feeb8930
adiozdaniel <adiozdaniel@gmail.com>
2024-09-10 11:04:35 +0300

    ref: implement fetchdata - to avoid repetition

--- frontend/views/static/scripts/fetchdata.js
+++ frontend/views/static/scripts/fetchdata.js
@@ -1,18 +1,20 @@
-const url = "http://localhost:4000/api/getallartists";
+const baseUrl = "http://localhost:4000/api/";
 const headers = new Headers({
 	"Content-Type": "application/json",
 });
-const body = JSON.stringify({});
 
 /**
- * Fetches all artists' data from the server
- * @param {JSON} body - an empty json body object
- * @returns {Promise<Object>} - The JSON response from the server containing artists' data
+ * Utility function to fetch data from the server
+ * @param {string} endpoint - The API endpoint to send the request to
+ * @param {Object} [bodyData] - Optional body data to send with the request
+ * @returns {Promise<Object>} - The JSON response from the server
  * Throws an error if the fetch request fails or the response is not ok.
  */
-export const getAllArtists = async () => {
+const fetchData = async (endpoint, bodyData = {}) => {
+	const body = JSON.stringify(bodyData);
+
 	try {
-		const response = await fetch(url, {
+		const response = await fetch(`${baseUrl}${endpoint}`, {
 			method: "POST",
 			headers,
 			body,
@@ -24,34 +26,24 @@ export const getAllArtists = async () => {
 
 		return await response.json();
 	} catch (error) {
-		console.error("Error fetching artists:", error);
+		console.error(`Error fetching data from ${endpoint}:`, error);
 		throw error;
 	}
+};
+
+/**
+ * Fetches all artists' data from the server
+ * @returns {Promise<Object>} - The JSON response from the server containing artists' data
+ */
+export const getAllArtists = () => {
+	return fetchData("getallartists");
 };
 
 /**
  * Fetches data for a specific artist by their ID
  * @param {string} id - The ID of the artist to fetch
  * @returns {Promise<Object>} - The JSON response from the server containing the artist's data
- * Throws an error if the fetch request fails or the response is not ok.
  */
-export const getArtistById = async (id) => {
-	const body = JSON.stringify({ artist_id: id });
-
-	try {
-		const response = await fetch(url, {
-			method: "POST",
-			headers,
-			body,
-		});
-
-		if (!response.ok) {
-			throw new Error(`HTTP error! status: ${response.status}`);
-		}
-
-		return await response.json();
-	} catch (error) {
-		console.error("Error fetching artist:", error);
-		throw error;
-	}
+export const getArtistById = (id) => {
+	return fetchData("getartistbyid", { artist_id: id });
 };
