7b5a6b372d848c0207d4488f4599664495529049
adiozdaniel <adiozdaniel@gmail.com>
2024-09-29 17:19:17 +0300

    chore: add concertlocation

--- backend/internals/handlers/handlers.go
+++ backend/internals/handlers/handlers.go
@@ -2,10 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
 
 	"github.com/Doreen-Onyango/groupie-tracker-api/internals/models"
 	"github.com/Doreen-Onyango/groupie-tracker-api/internals/responses"
@@ -79,7 +76,7 @@ func (m *Repo) GetArtistById(w http.ResponseWriter, r *http.Request) {
 }
 
 // API handler to get artist concert locations with geocoding
-func (m *Repo) GetConcertLocations(w http.ResponseWriter, r *http.Request) {
+func (m *Repo) GetConcertLocation(w http.ResponseWriter, r *http.Request) {
 	var requestData struct {
 		Request string `json:"artist_id"`
 	}
@@ -90,26 +87,20 @@ func (m *Repo) GetConcertLocations(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	artistData, err := m.app.Res.GetArtistById(requestData.Request)
-	if err != nil || len(artistData) == 0 {
+	loc, err := m.app.Res.GetCoordinates(requestData.Request)
+	if err != nil {
 		m.res.Err = true
 		m.res.Message = err.Error()
 		m.res.ErrJSON(w, err, http.StatusNotFound)
 		return
 	}
 
-	// Geocode concert locations
-	for i, concert := range artistData["locations"] {
-		lat, lng, err := GeocodeLocation(concert.Location)
-		if err != nil {
-			http.Error(w, "Failed to geocode location", http.StatusInternalServerError)
-			return
-		}
-		artistData[i].Latitude = lat
-		artistData[i].Longitude = lng
-	}
+	locJSON, _ := json.Marshal(loc)
+	m.res.Err = false
+	m.res.Message = "success"
+	m.res.Data = json.RawMessage(locJSON)
 
-	// Return the artist's concert data with geolocation
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(artist)
+	if err := m.res.WriteJSON(w, *m.res, http.StatusOK); err != nil {
+		m.res.ErrJSON(w, err, http.StatusInternalServerError)
+	}
 }
