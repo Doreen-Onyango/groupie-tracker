fa91052a99ef286ade48366f35100ac4540e5779
adiozdaniel <adiozdaniel@gmail.com>
2024-10-14 17:52:05 +0300

    chore:add a 404 handler

--- frontend/internals/handlers/home.go
+++ frontend/internals/handlers/home.go
@@ -11,3 +11,7 @@ func (h *Repo) HomeHandler(w http.ResponseWriter, r *http.Request) {
 func (h *Repo) AboutHandler(w http.ResponseWriter, r *http.Request) {
 	h.res.RenderTemplate("about.page.html", http.StatusOK, w)
 }
+
+func (h *Repo) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	h.res.RenderTemplate("404.page.html", http.StatusNotFound, w)
+}
