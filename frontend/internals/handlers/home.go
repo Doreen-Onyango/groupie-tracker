cfbf7c6cb1e8bf1b24d0d99098974c90c9dd18a9
doonyango <datieno001@gmail.com>
2024-10-14 16:05:42 +0300

    refactor: add a about handler

--- frontend/internals/handlers/home.go
+++ frontend/internals/handlers/home.go
@@ -7,3 +7,7 @@ import (
 func (h *Repo) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	h.res.RenderTemplate("home.page.html", http.StatusOK, w)
 }
+
+func (h *Repo) AboutHandler(w http.ResponseWriter, r *http.Request) {
+	h.res.RenderTemplate("about.page.html", http.StatusOK, w)
+}
