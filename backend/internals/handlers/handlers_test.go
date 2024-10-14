2b78d0c32fcfe79b0823e41a25aecb4c9f1f420b
doonyango <datieno001@gmail.com>
2024-10-14 13:57:02 +0300

    test: add test for get artist by id function

--- backend/internals/handlers/handlers_test.go
+++ backend/internals/handlers/handlers_test.go
@@ -3,9 +3,8 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
-	"testing"
-
 	"strings"
+	"testing"
 
 	"github.com/Doreen-Onyango/groupie-tracker-api/internals/models"
 	"github.com/Doreen-Onyango/groupie-tracker-api/internals/responses"
@@ -63,6 +62,63 @@ func TestRepo_GetAllArtists(t *testing.T) {
 			// Check the response body by trimming whitespaces
 			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.wantBody) {
 				t.Errorf("GetAllArtists() body = %v, want %v", rr.Body.String(), tt.wantBody)
+			}
+		})
+	}
+}
+
+func TestRepo_GetArtistById(t *testing.T) {
+	type fields struct {
+		app *models.App
+		res *responses.JSONRes
+	}
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		args       args
+		wantStatus int
+		wantBody   string
+	}{
+		{
+			name: "Invalid method",
+			fields: fields{
+				app: &models.App{},
+				res: &responses.JSONRes{},
+			},
+			args: args{
+				w: httptest.NewRecorder(),
+				r: httptest.NewRequest("POST", "/artist?id=1", nil),
+			},
+			wantStatus: http.StatusNotFound,
+			wantBody:   `{"error":"Artist not found"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &Repo{
+				app: tt.fields.app,
+				res: tt.fields.res,
+			}
+
+			// Call the handler
+			m.GetArtistById(tt.args.w, tt.args.r)
+
+			// Get the response recorder
+			rr := tt.args.w.(*httptest.ResponseRecorder)
+
+			// Check the status code
+			if status := rr.Code; status != tt.wantStatus {
+				t.Errorf("GetArtistById() status code = %v, want %v", status, tt.wantStatus)
+			}
+
+			// Check the response body by trimming whitespaces
+			if strings.TrimSpace(rr.Body.String()) != strings.TrimSpace(tt.wantBody) {
+				t.Errorf("GetArtistById() body = %v, want %v", rr.Body.String(), tt.wantBody)
 			}
 		})
 	}
