ad5388dd2ce2afc5d5fc053dd586b9ec9c69aa5c
adiozdaniel <adiozdaniel@gmail.com>
2024-10-14 18:17:34 +0300

    feat:add route middleware - to redirect unauthorized requests

--- backend/internals/middlewares/cors.go
+++ backend/internals/middlewares/cors.go
@@ -1,6 +1,8 @@
 package middlewares
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // Setting access controls for the project.
 func CORSMiddleware(next http.Handler) http.Handler {
@@ -9,11 +11,29 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if r.Method == http.MethodOptions{
+		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
-			return 
+			return
 		}
 
 		next.ServeHTTP(w, r)
+	})
+}
+
+var allowedRoutes = map[string]bool{
+	"/api/getallartists":  true,
+	"/api/getartistbyid":  true,
+	"/api/getcoordinates": true,
+}
+
+// RouteChecker is a middleware that checks allowed routes
+func RouteChecker(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if _, ok := allowedRoutes[req.URL.Path]; !ok {
+			http.Redirect(w, req, "http://localhost:8080/not-allowed", http.StatusFound)
+			return
+		}
+
+		next.ServeHTTP(w, req)
 	})
 }
