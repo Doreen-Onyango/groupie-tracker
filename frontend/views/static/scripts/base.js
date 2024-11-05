5f58e90cf9c13105848756f309ce5cf969e2348b
adiozdaniel <adiozdaniel@gmail.com>
2024-11-05 07:49:24 +0300

    fix: keep the search icon at the center always - for user experience

--- frontend/views/static/scripts/base.js
+++ frontend/views/static/scripts/base.js
@@ -8,14 +8,18 @@ export default class Base {
 
 Base.prototype.toggleSearch = function () {
 	const searchIcon = document.querySelector(".search-icon");
+	const searchBox = document.querySelector(".search-box");
+	const searchInput = document.getElementById("searchUnified");
+
 	searchIcon.onclick = function toggleSearch() {
-		const searchInput = document.getElementById("searchUnified");
 		searchInput.classList.toggle("expanded");
 		searchInput.focus();
 
 		if (searchInput.classList.contains("expanded")) {
 			searchIcon.style.transition = "margin-top 0.7s ease-in-out";
+			searchBox.style.width ="100%";
 		} else {
+			searchBox.style.width = "0";
 			searchIcon.style.transition = "margin-top 0.5s ease-out";
 		}
 	};
